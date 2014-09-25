684b7905c15e1e0d52db0e3c9a27eb8a1fc5c7e2
pimterry <pimterry@gmail.com>
2014-09-25 20:03:17 +0100

    Improve initial data, now that we don't track facets

--- comparably/src/ItemViewModel.js
+++ comparably/src/ItemViewModel.js
@@ -2,7 +2,7 @@ window.ItemViewModel = (function () {
     var ItemFacetViewModel = (function () {
         function ItemFacetViewModel(itemFacet) {
             this.description = ko.observable();
-            this.score = ko.observable();
+            itemFacet.score = itemFacet.score || 50;
             ko.mapping.fromJS(itemFacet, {}, this);
         }
 
@@ -11,12 +11,13 @@ window.ItemViewModel = (function () {
 
     var ItemViewModel = function ItemViewModel(item) {
         item.name = item.name || "?";
-        this.facets = ko.observableArray();
+        item.facets = (item.facets || []).map(function (facet) {
+            return new ItemFacetViewModel(facet);
+        });
         ko.mapping.fromJS(item, {}, this);
     };
 
     ItemViewModel.prototype.addFacet = function (facet) {
-        facet.score = facet.score === undefined ? 50 : facet.score;
         this.facets.push(new ItemFacetViewModel(facet));
     }
 
